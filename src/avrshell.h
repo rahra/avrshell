3e702a381a89d5021484afa5285192474c8f0268
rahra <bf@abenteuerland.at>
2019-02-13 16:49:59 +0100

    added register_int()

--- src/avrshell.h
+++ src/avrshell.h
@@ -9,5 +9,14 @@
 #define PROGMEM __attribute__((__progmem__))
 #endif
 
+#define NUM_INT_VECTS 26
+
+#ifndef __ASSEMBLER__
+#include <stdint.h>
+
+int8_t register_int(int8_t, void (*)(void));
+
+#endif
+
 #endif
 

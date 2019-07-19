8c8710fbfd69634d4aac8edd6b13b9bc48a6518d
rahra <bf@abenteuerland.at>
2019-07-19 20:03:23 +0200

    duplicate SEI, added function sys_peek_serial()

--- src/serial_io.h
+++ src/serial_io.h
@@ -11,6 +11,7 @@ uint8_t sys_read(char *, uint8_t);
 uint8_t sys_write(const char *, uint8_t);
 uint8_t sys_pwrite(const char *, uint8_t);
 void sys_send(char);
+uint8_t sys_peek_serial(void);
 
 
 #endif
